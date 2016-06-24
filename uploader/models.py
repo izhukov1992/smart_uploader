728d5edfb97c462097e04f2b62cd318c38acf3ad
Ilya Zhukov <izhukov1992@gmail.com>
2016-06-25 02:38:55 +0300

    Upload files to folder. Ignore uploads

--- uploader/models.py
+++ uploader/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class StorageFile(models.Model):
     """uploader.StorageFile"""
 
-    file = models.FileField()
+    file = models.FileField(upload_to='uploads')
     sha1 = models.CharField(max_length=255, unique=True)
