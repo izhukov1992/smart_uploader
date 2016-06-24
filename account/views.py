cc9b78a0287a4b2184dca34b6a86e7a1421bce13
Ilya Zhukov <izhukov1992@gmail.com>
2016-06-25 02:27:21 +0300

    Don't save duplicate files. Add alerts to page

--- account/views.py
+++ account/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from django.core.context_processors import csrf
 from django.views.generic.base import TemplateView
+from uploader.models import StorageFile
 from uploader.forms import StorageFileForm
+from uploader.utils import getSHA1Digest
 
 
 class BaseView(TemplateView):
     """account.BaseView"""
 
-    context = dict()
+    context = {}
 
     def get(self, request):
         return render(request, self.template_name, self.context)
@@ -18,8 +21,32 @@ class IndexView(BaseView):
     template_name = 'index.html'
 
     def get(self, request):
+        self.context = {}
         form = StorageFileForm()
-        self.context['form'] = form
+        self.context.update(csrf(request))
+        self.context.update({'form': form})
+        return super(IndexView, self).get(request)
+
+    def post(self, request):
+        form = StorageFileForm(request.POST, request.FILES)
+        if not form.is_valid():
+            self.context.update({'form': form})
+            return super(IndexView, self).get(request)
+            
+        file_uploaded = request.FILES['file']
+        hash = getSHA1Digest(file_uploaded)
+        try:
+            file_duplicate = StorageFile.objects.get(sha1=hash)
+        except:
+            file_duplicate = None
+        if file_duplicate:
+            self.context.update({'file_duplicate': file_duplicate})
+            file_new = file_duplicate
+        else:
+            file_new = StorageFile(file=file_uploaded, sha1=hash)
+            file_new.save()
+            self.context.update({'file_new': file_new})
+        #user area file instance creation
         return super(IndexView, self).get(request)
 
 
