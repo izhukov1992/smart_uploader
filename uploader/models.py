3d0f4996a24d2d9a33cb7e3d4ab8973dc8c18cac
Ilya Zhukov <izhukov1992@gmail.com>
2016-06-24 22:45:46 +0300

    Add StorageFile model

--- uploader/models.py
+++ uploader/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class StorageFile(models.Model):
+    """uploader.StorageFile"""
+
+    file = models.FileField()
+    sha1 = models.CharField(max_length=255, unique=True)
