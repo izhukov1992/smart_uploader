e891a3c114598e0dcbfd74da76e5af45f8f00199
Ilya Zhukov <izhukov1992@gmail.com>
2016-06-26 23:41:48 +0300

    Simplify registration of models in admin area

--- account/admin.py
+++ account/admin.py
@@ -1,12 +1,4 @@
 from django.contrib import admin
 from .models import UserFile
 
-
-class UserFileAdmin(admin.ModelAdmin):
-    """account.UserFileAdmin"""
-
-    pass
-
-
-admin.site.register(UserFile, UserFileAdmin)
-
+admin.site.register(UserFile, admin.ModelAdmin)
--- uploader/admin.py
+++ uploader/admin.py
@@ -1,11 +1,4 @@
 from django.contrib import admin
 from .models import StorageFile
 
-
-class StorageFileAdmin(admin.ModelAdmin):
-    """uploader.StorageFileAdmin"""
-
-    pass
-
-
-admin.site.register(StorageFile, StorageFileAdmin)
+admin.site.register(StorageFile, admin.ModelAdmin)
