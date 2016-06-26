8d14341ff2cdf444c62e0564f757fed6cd41fe40
Ilya Zhukov <izhukov1992@gmail.com>
2016-06-26 17:06:56 +0300

    Define media root and upload files to media root by default

--- uploader/models.py
+++ uploader/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class StorageFile(models.Model):
     """uploader.StorageFile"""
 
-    file = models.FileField(upload_to='uploads')
+    file = models.FileField()
     sha1 = models.CharField(max_length=255, unique=True)
