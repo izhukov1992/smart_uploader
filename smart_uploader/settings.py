8162cb4068308dde447322cd86f8e3cefb9c2fdb
Ilya Zhukov <izhukov1992@gmail.com>
2017-12-20 02:40:46 +0300

    Split heroku and local version. Local is on master

--- smart_uploader/settings.py
+++ smart_uploader/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,9 +81,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-# Find PostgreSQL configurations
-DATABASES['default'] =  dj_database_url.config()
 
 
 # Password validation
